72e7fc1936d9a2998b7ea5e480db56d7fa654928
Darshan <mr.darshanghorpade@gmail.com>
2022-06-02 11:13:28 +0530

    Adding UI Blog App Files

---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,117 @@
+import { Link } from 'react-router-dom';
+import './singlePost.css';
+
+export default function SinglePost() {
+  return (
+    <div className='singlePost'>
+        <div className='singlePostWrapper'>
+            <img src='https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' alt='' className='singlePostImg' />
+            <h1 className='singlePostTitle'>
+                Lorem ipsum dolor sit amet.
+                <div className='singlePostEdit'>
+                    <i className="singlePostIcon far fa-trash-alt"></i>
+                    <i className="singlePostIcon far fa-edit"></i>
+                </div>
+            </h1>
+            <div className='singlePostInfo'>
+                <span className='singlePostAuthor'>
+                    Author: 
+                    <b>
+                        <Link className='link' to='/posts?username=Safak'>
+                             Safak
+                        </Link>
+                    </b>
+                </span>
+                <span className='singlePostDate'>1 hour ago</span>
+            </div>
+            <p className='singlePostDesc'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            <br /><br />
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+            fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+            atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            </p>
+        </div>
+    </div>
+  )
+}
